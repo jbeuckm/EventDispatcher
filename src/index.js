05f453b568e6b37109c88ef24b37e0b7bb7e16a3
j.beuckman <j.beuckman@klarna.com>
2018-12-29 11:56:15 +0100

    add sources

---
+++ src/index.js
@@ -0,0 +1,57 @@
+class EventDispatcher {
+  constructor(types) {
+    this.types = types
+    this.listeners = {}
+
+    for (let type of types) {
+      this.listeners[type] = []
+    }
+  }
+
+  validateType = type => {
+    if (this.types.indexOf(type) === -1) {
+      throw new Error(
+        `EventDispatcher child class ${this.constructor.name} has no event of type "${type}".`
+      )
+    }
+  }
+
+  addEventListener = (type, listener) => {
+    this.validateType(type)
+
+    if (this.listeners[type].indexOf(listener) === -1) {
+      this.listeners[type].push(listener)
+    } else {
+      console.log(
+        `EventDispatcher child class ${
+          this.constructor.name
+        } will not add redundant listener for event "${type}".`
+      )
+    }
+  }
+
+  removeEventListener = (type, listener) => {
+    this.validateType(type)
+
+    const listenerIndex = this.listeners[type].indexOf(listener)
+    if (listenerIndex === -1) {
+      throw new Error(
+        `EventDispatcher child class ${
+          this.constructor.name
+        } has no listener ${listener} of type ${type}.`
+      )
+    } else {
+      this.listeners[type].splice(listenerIndex, 1)
+    }
+  }
+
+  dispatchEvent = (type, data) => {
+    this.validateType(type)
+
+    for (let listener of this.listeners[type]) {
+      listener({ type, data })
+    }
+  }
+}
+
+export default EventDispatcher
---
+++ src/index.spec.js
@@ -0,0 +1,62 @@
+import EventDispatcher from './index'
+
+const TEST_EVENT_TYPE = 'TEST_EVENT_TYPE'
+
+describe('EventDispatcher', () => {
+  it('will not add listener for unknown event type', done => {
+    const ED = new EventDispatcher([TEST_EVENT_TYPE])
+
+    try {
+      ED.addEventListener('💣', () => {})
+    } catch (e) {
+      done()
+    }
+  })
+
+  it('can dispatch known event types', () => {
+    const ED = new EventDispatcher([TEST_EVENT_TYPE])
+
+    const myListener = jest.fn()
+
+    ED.addEventListener(TEST_EVENT_TYPE, myListener)
+
+    ED.dispatchEvent(TEST_EVENT_TYPE, 123)
+
+    expect(myListener).toHaveBeenCalled()
+  })
+
+  it('event listener receives type and data for an event', done => {
+    const ED = new EventDispatcher([TEST_EVENT_TYPE])
+
+    ED.addEventListener(TEST_EVENT_TYPE, ({ type, data }) => {
+      expect(type).toEqual(TEST_EVENT_TYPE)
+      expect(data).toEqual(123)
+      done()
+    })
+
+    ED.dispatchEvent(TEST_EVENT_TYPE, 123)
+  })
+
+  it('can not remove an unknown listener', done => {
+    const ED = new EventDispatcher([TEST_EVENT_TYPE])
+
+    try {
+      ED.removeEventListener(TEST_EVENT_TYPE, console.log)
+    } catch (e) {
+      done()
+    }
+  })
+
+  it('can remove a previously added listener', () => {
+    const ED = new EventDispatcher([TEST_EVENT_TYPE])
+
+    const myListener = jest.fn()
+
+    ED.addEventListener(TEST_EVENT_TYPE, myListener)
+    ED.removeEventListener(TEST_EVENT_TYPE, myListener)
+
+    ED.dispatchEvent(TEST_EVENT_TYPE)
+
+    expect(myListener).not.toHaveBeenCalled()
+  })
+})
